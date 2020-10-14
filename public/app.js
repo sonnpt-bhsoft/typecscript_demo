cdfc285d7f3e5bc71a8e8262af19b2b79603345a
Thai Son <sonnpt.bhsoft@gmail.com>
2020-10-14 16:20:39 +0700

    shorthand inittialize class, using readonly-private-public to limit access/modify values

--- public/app.js
+++ public/app.js
@@ -1,4 +1,26 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(client, // read only both outside/inside but not change value
+    details, // change inside of the class not outside
+    amount // default: can change and read the value outside/inside of the class
+    ) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invoiceOne = new Invoice('mario', 'working on the mario website', 250);
+var invoiceTwo = new Invoice('luigi', 'working on the luigi website', 300);
+var invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.amount, inv.format());
+});
 // const form = document.querySelector('form')!
 var form = document.querySelector('.new-item-form');
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
+class Invoice { 
+    constructor(
+        readonly client: string, // read only both outside/inside but not change value
+        private details: string, // change inside of the class not outside
+        public amount: number // default: can change and read the value outside/inside of the class
+    ){}
+
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+
+const invoiceOne = new Invoice( 'mario', 'working on the mario website', 250);
+const invoiceTwo = new Invoice( 'luigi', 'working on the luigi website', 300)
+
+let invoices: Invoice[] = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+})
+
 // const form = document.querySelector('form')!
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
-
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
